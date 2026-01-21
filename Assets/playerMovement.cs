5e9bf67fb822bf47706b2994a725061d916b724b
baihaqiiiii <pancigosong94@gmail.com>
2026-01-21 22:10:55 +0800

    project start

---
+++ Assets/playerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //neww
+
+public class playerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody2D Rb; //inialisasi
+    public float jumpforce; //Jump loncat
+    float score;
+
+    public Text scoreText; //neww
+    void Start()
+    {
+        Rb = GetComponent<Rigidbody2D>(); //perkenalkan inialisasi
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "" + score; //neww
+        if (Input.GetMouseButtonDown(0)) //klik mouse kiri for pc / tap untuk hp
+        {
+            Rb.velocity = Vector2.up * jumpforce;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "point")
+        {
+            score++;
+        }
+        if(collision.gameObject.tag =="pipa")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
